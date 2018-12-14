a80780d0dabee127884626be893bf7bd4b1a42ad
unknown <claudiamalasquez67@gmail.com>
2018-12-13 21:40:54 -0500

    Agregar la función getDataMainOfPokemon dentro del objeto pokemon

--- src/data.js
+++ src/data.js
@@ -1,8 +1,14 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+window.pokemon = {
+ //Obtener Data principal de Pokemon
+ getDataMainOfPokemon: function(arr){
+ 	const newArr = [];
+ 	for(let i = 0; i < arr.length; i++){
+ 		newArr.push({name : arr[i].name, img: arr[i].img, type: arr[i].type, height: arr[i].height, weight: arr[i].weight, weaknesses: arr[i].weaknesses});
+ 	}
+ 	return newArr;
+ }
+};
