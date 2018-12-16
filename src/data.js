d14c6a4c08d2272d6758c6bb8dbf4e984da1ea66
unknown <claudiamalasquez67@gmail.com>
2018-12-15 22:36:32 -0500

    Cambio de recorrido de la data, for por forEach

--- src/data.js
+++ src/data.js
@@ -1,14 +1,12 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
-
 window.pokemon = {
  //Obtener Data principal de Pokemon
  getDataMainOfPokemon: function(arr){
  	const newArr = [];
- 	for(let i = 0; i < arr.length; i++){
- 		newArr.push({name : arr[i].name, img: arr[i].img, type: arr[i].type, height: arr[i].height, weight: arr[i].weight, weaknesses: arr[i].weaknesses});
- 	}
+ 	arr.forEach(function(element){
+ 		newArr.push({name : element.name, img: element.img, type: element.type, height: element.height, weight: element.weight, weaknesses: element.weaknesses});
+ 	});
  	return newArr;
- }
+ },
 };
