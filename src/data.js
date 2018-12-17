5f7a8531878a9b2611935342c31968d289368848
MirellaLucero <lucero_200900@hotmail.com>
2018-12-17 08:38:43 -0500

    cambios para gh-pages

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-// const example = () => {
-//   return 'example';
-// };
-//
-// window.example = example;
-
-function mostrarData(){
-  let arrPokemon= POKEMON.pokemon;
-  for(let i=0; i<arrPokemon.length;i++){
-    return arrPokemon[i].name;
-}
-}
+window.pokemon = {
+  //Obtener data principal de Pokemon (Historia de usuario #01)
+  getDataMainOfPokemon: function(arr){
+    const newArr = [];
+    arr.forEach(function(element){
+      newArr.push({name : element.name, img: element.img, type: element.type, height: element.height, weight: element.weight, weaknesses: element.weaknesses});
+    });
+    return newArr;
+  },
+ };
+ 
+ 
