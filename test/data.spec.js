deb510e69fe904cc5681b0e21dc595455d551c01
unknown <claudiamalasquez67@gmail.com>
2018-12-18 13:04:20 -0500

    Implementando el test del objeto pokemon

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,18 @@
 require('../src/data.js');
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe('pokemon', () => {
+  it('debería retornar un objeto', () => {
+    expect(typeof pokemon).toBe('object');
   });
 });
+
+
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
+
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
