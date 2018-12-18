f8dedffd274ad5522bc12638eb45686d950c76f1
MirellaLucero <lucero_200900@hotmail.com>
2018-12-18 10:37:35 -0500

    cambiando nombre de pokemones a mayúsculas,agregando rules al eslintrc, cambiando test

--- src/data.js
+++ src/data.js
@@ -2,14 +2,14 @@
 // puedes ver como agregamos la función a nuestro objeto global window
 
 window.pokemon = {
-  //Obtener data principal de Pokemon (Historia de usuario #01)
-  getDataMainOfPokemon: function(arr){
+  // Obtener data principal de Pokemon (Historia de usuario #01)
+  getDataMainOfPokemon: function(arr) {
     const newArr = [];
-    arr.forEach(function(element){
-      newArr.push({name : element.name, img: element.img, type: element.type, height: element.height, weight: element.weight, weaknesses: element.weaknesses});
+    arr.forEach(function(element) {
+      newArr.push({name: element.name, img: element.img, type: element.type, height: element.height, weight: element.weight, weaknesses: element.weaknesses});
     });
     return newArr;
   },
- };
+};
  
  
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,12 @@
 require('../src/data.js');
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('pokemon', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof pokemon).toBe('object');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+  // it('returns `example`', () => {
+  //   expect(example()).toBe('example');
+  // });
 });
